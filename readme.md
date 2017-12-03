24f0ca54dd7c8ea99372869a838b9994a1b9af0f
proyectosbeta <josego85@gmail.com>
2017-12-02 22:02:34 -0300

    API GEO de Paraguay. Ahora se puede obtener el departamento de Paraguay pasando longitud y latitud.

---
+++ readme.md
@@ -0,0 +1,20 @@
+## Instalacion
+    npm install
+
+## Configuracion (base de datos MySQL)
+server.js
+
+        host: 'localhost',
+        user: 'root',
+        password : 'root',
+        port : 3306, // Puerto mysql
+        database: 'paraguay'
+
+
+
+You're gonna need to create a DB named 'test' or whatever you name it,  import t_user.sql
+
+
+## Abrir navegador web
+* http://localhost:3000/api/v1/departamentos
+* http://localhost:3000/api/v1/departamento/-56.987/-25.564
---
+++ server.js
@@ -0,0 +1,116 @@
+var express  = require('express'),
+    path     = require('path'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+app.use(bodyParser.urlencoded({ extended: true })); //support x-www-form-urlencoded
+app.use(bodyParser.json());
+
+/*
+    Conexion a MySql.
+*/
+var connection  = require('express-myconnection'),
+    mysql = require('mysql');
+
+app.use(
+    connection(mysql, {
+        host     : 'localhost',
+        user     : 'root',
+        password : '123456',
+        database : 'paraguay',
+        debug    : false    // Si es true es para el debug.
+    }, 'request')
+);
+
+// RESTful route.
+var v1 = express.Router();
+
+v1.get('/', function(req, res){
+    res.send('API Paraguay - GIS');
+});
+
+
+/*------------------------------------------------------
+*  This is router middleware,invoked everytime
+*  we hit url /api and anything after /api
+*  like /api/user , /api/user/7
+*  we can use this for doing validation,authetication
+*  for every route started with /api
+--------------------------------------------------------*/
+v1.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
+var curut = v1.route('/departamentos');
+
+
+//show the CRUD interface | GET
+// Ejemplo: http://localhost:3000/api/v1/departamentos
+curut.get(function(req, res, next){
+    req.getConnection(function(err,conn){
+        if (err){
+            return next("No se puede conectar a la base de datos.");
+        }
+        var query = conn.query('SELECT departamento_id, departamento_nombre, departamento_capital FROM departamentos', function(err, rows){
+            if(err){
+                console.log(err);
+                return next("Error Mysql, check la consulta.");
+            }
+            res.json(rows);
+         });
+    });
+});
+
+//now for Single route (GET)
+var curut2 = v1.route('/departamento/:log/:lat');
+
+/*------------------------------------------------------
+route.all is extremely useful. you can use it to do
+stuffs for specific routes. for example you need to do
+a validation everytime route /api//departamento/:log/:lat it hit.
+
+remove curut2.all() if you dont want it
+------------------------------------------------------*/
+curut2.all(function(req, res, next){
+    console.log(req.params);
+    next();
+});
+
+//get data to update
+// Ejemplo: http://localhost:3000/api/v1/departamento/-56.987/-25.564
+curut2.get(function(req, res, next){
+    var log = req.params.log;
+    var lat = req.params.lat;
+
+    req.getConnection(function(err, conn){
+        if (err){
+            return next("No se puede conectar a la base de datos.");
+        }
+        var query = conn.query("SELECT departamento_id, departamento_nombre, departamento_capital FROM departamentos WHERE ST_CONTAINS(geom, POINT(" + log + "," + lat + "))", function(err, rows){
+            if(err){
+                console.log(err);
+                return next("Error Mysql, check la consulta.");
+            }
+
+            // if departemento not found
+            if(rows.length < 1){
+                return res.send("Departamento no encontrado!!!");
+            }
+            res.json(rows);
+        });
+    });
+});
+
+// Route principal:
+// - /api/v1
+app.use('/api/v1', v1);
+
+// Start Server.
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    host = (host === '::' ? 'localhost' : host);
+    var port = server.address().port;
+
+    console.log('Escucha http://%s:%s', host, port);
+});
