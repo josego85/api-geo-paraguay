02ed53f9bc64ec497bdd81eacdf8e19cf9c6cb30
josego <josego85@gmail.com>
2021-02-27 12:37:51 -0300

    Refactoring code.

--- app/app.js
+++ app/app.js
@@ -7,46 +7,18 @@ const helmet = require("helmet");
 const cors = require("cors");
 const app = express();
 
-// Parse requests of content-type: application/json
-app.use(bodyParser.json());
-
-// Parse requests of content-type: application/x-www-form-urlencoded
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
+app.use(bodyParser.json());
 
-// Seguridad.
+// Security.
 app.use(helmet());
 app.use(cors());
 
-// Register the routes.
+// Register the API REST routes.
 app.use("/api/v1", rest);
 
 module.exports = app;
-
-// Funcion para obtener la IP.
-// function getIP(req) {
-//     var ip = req.headers['x-forwarded-for'] ||
-//       req.connection.remoteAddress ||
-//       req.socket.remoteAddress ||
-//       req.connection.socket.remoteAddress;
-//     ip = ip.split(',')[0];
-//     ip = ip.split(':').slice(-1); // En caso que la IP retorne en el formato: "::ffff:146.xxx.xxx.xxx"
-//     return ip;
-// }
-
-/*------------------------------------------------------
-*  This is router middleware,invoked everytime
-*  we hit url /api and anything after /api
-*  like /api/user , /api/user/7
-*  we can use this for doing validation,authetication
-*  for every route started with /api
---------------------------------------------------------*/
-// v1.use(function(req, res, next) {
-//     var ip = getIP(req);
-//     console.log("Cliente IP es: " + ip);
-//     console.log(req.method, req.url);
-//     next();
-// });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,26 +1,26 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const departments = require('../controllers/department.controller.js');
-const distrits = require('../controllers/distrit.controller.js');
-const cities = require('../controllers/city.controller.js');
-const neighborhoods = require('../controllers/neighborhood.controller.js');
+const departments = require("../controllers/department.controller.js");
+const distrits = require("../controllers/distrit.controller.js");
+const cities = require("../controllers/city.controller.js");
+const neighborhoods = require("../controllers/neighborhood.controller.js");
 
 // All the routes.
-router.get('/paraguay/:lng/:lat', departments.findByLngLat);
+router.get("/paraguay/:lng/:lat", departments.findByLngLat);
 
 // Departments.
-router.get('/departamentos', departments.findAll);
-router.get('/departamentos/:lng/:lat', departments.findByLngLat);
+router.get("/departamentos", departments.findAll);
+router.get("/departamentos/:lng/:lat", departments.findByLngLat);
 
 // Distrits.
-router.get('/distritos', distrits.findAll);
+router.get("/distritos", distrits.findAll);
 
 // Cities.
-router.get('/ciudades', cities.findAll);
+router.get("/ciudades", cities.findAll);
 
 // Neighborhoods.
-router.get('/barrios', neighborhoods.findAll);
+router.get("/barrios", neighborhoods.findAll);
 
 module.exports = router;
