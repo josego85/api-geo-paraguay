730ba8a8c7dd46fc325ede1d0c04e45ebe16c79a
josego <josego85@gmail.com>
2025-05-05 22:37:54 +0200

    feat: added pagination support to the `getCities` method in the City controller

--- app/controllers/city.controller.js
+++ app/controllers/city.controller.js
@@ -1,24 +1,25 @@
-const cacheService = require('services/cacheService');
 const City = require('models/city.model');
+const cityService = require('services/cityService');
 
-exports.getCities = async (request, response) => {
+exports.getCities = async (req, res) => {
   try {
-    const data = await City.findAll(request.sorting);
-    if (!data) {
-      return response.status(404).send({ message: 'Cities not found' });
+    const { page, limit, sortField, sortOrder } = req.processedQuery;
+    const options = {
+      page,
+      limit,
+      sort: { field: sortField, order: sortOrder },
+    };
+
+    const data = await cityService.findAll(options);
+
+    if (!data || data.length === 0) {
+      return res.status(404).send({ message: 'Cities not found' });
     }
 
-    // Update cache if middleware provided cache info
-    if (request.cacheInfo) {
-      cacheService
-        .set(request.cacheInfo.key, data)
-        .catch((error) => console.error('Cache Error:', error));
-    }
-
-    return response.status(200).json({ data });
+    return res.status(200).json({ data, page, limit });
   } catch (error) {
-    return response.status(500).send({
-      message: request.polyglot.t('failed_to_retrieve_cities') || error.message,
+    return res.status(500).send({
+      message: req.polyglot?.t('failed_to_retrieve_cities') || error.message,
     });
   }
 };
@@ -40,24 +41,24 @@ exports.getCities = async (request, response) => {
 //   });
 // };
 
-exports.getCityById = async (request, response) => {
+exports.getCityById = async (req, res) => {
   try {
-    const { id } = request.params;
+    const { id } = req.params;
 
     if (!id) {
-      return response.status(400).send({ message: 'ID is required' });
+      return res.status(400).send({ message: 'ID is required' });
     }
 
     const data = await City.findById(id);
 
     if (!data) {
-      return response.status(404).send({ message: 'City not found' });
+      return res.status(404).send({ message: 'City not found' });
     }
 
-    return response.status(200).json(data);
+    return res.status(200).json(data);
   } catch (error) {
-    return response.status(403).send({
-      message: request.polyglot?.t('failed_to_retrieve_city') || error.message,
+    return res.status(403).send({
+      message: req.polyglot?.t('failed_to_retrieve_city') || error.message,
     });
   }
 };
