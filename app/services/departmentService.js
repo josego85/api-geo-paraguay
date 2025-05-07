456ccff55d6114cb420658f6851c9f2dd02486e9
josego <josego85@gmail.com>
2025-05-07 12:29:26 +0200

    refactor: city, department, district and neighborhood service

--- app/services/departmentService.js
+++ app/services/departmentService.js
@@ -1,30 +1,4 @@
+const BaseService = require('services/baseService');
 const Department = require('models/department.model');
 
-const departmentService = {
-  async findAll({ page, limit, sortField, sortOrder, filter } = {}) {
-    const options = {
-      page,
-      limit,
-      sortField,
-      sortOrder,
-      filter,
-    };
-
-    return Department.findAll(options);
-  },
-
-  async findById(departmentId) {
-    return Department.findById(departmentId);
-  },
-
-  // validateCityData(cityData) {
-  //   if (!cityData.ciudad_nombre || typeof cityData.ciudad_nombre !== 'string') {
-  //     throw new Error('Invalid ciudad_nombre');
-  //   }
-  //   if (!Number.isInteger(cityData.distrito_id)) {
-  //     throw new Error('Invalid distrito_id');
-  //   }
-  // },
-};
-
-module.exports = departmentService;
+module.exports = new BaseService(Department);
--- app/services/neighborhoodService.js
+++ app/services/neighborhoodService.js
@@ -1,30 +1,4 @@
+const BaseService = require('services/baseService');
 const Neighborhood = require('models/neighborhood.model');
 
-const neighborhoodService = {
-  async findAll({ page, limit, sortField, sortOrder, filter } = {}) {
-    const options = {
-      page,
-      limit,
-      sortField,
-      sortOrder,
-      filter,
-    };
-
-    return Neighborhood.findAll(options);
-  },
-
-  async findById(neighborhoodId) {
-    return Neighborhood.findById(neighborhoodId);
-  },
-
-  // validateCityData(cityData) {
-  //   if (!cityData.ciudad_nombre || typeof cityData.ciudad_nombre !== 'string') {
-  //     throw new Error('Invalid ciudad_nombre');
-  //   }
-  //   if (!Number.isInteger(cityData.distrito_id)) {
-  //     throw new Error('Invalid distrito_id');
-  //   }
-  // },
-};
-
-module.exports = neighborhoodService;
+module.exports = new BaseService(Neighborhood);
