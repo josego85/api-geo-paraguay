8f970cac88b206d0c1e2f179209e54931d03348a
josego <josego85@gmail.com>
2025-05-08 20:07:32 +0200

    refactor: department model

--- app/models/department.model.js
+++ app/models/department.model.js
@@ -4,61 +4,6 @@ const pool = require('./db');
 const { SRID } = dbConfig;
 
 class Department {
-  static async findAll({ page = 1, limit = 10, sortField = 'id', sortOrder = 'ASC', ...filter }) {
-    try {
-      let query = 'SELECT dep.id, dep.name, dep.capital_name FROM department as dep';
-      const params = [];
-
-      // Apply filters
-      const filterConditions = [];
-      if (filter.name) {
-        filterConditions.push(`dep.name LIKE ?`);
-        params.push(`%${filter.name}%`);
-      }
-
-      if (filter.capital_name) {
-        filterConditions.push(`dep.capital_name LIKE ?`);
-        params.push(`%${filter.capital_name}%`);
-      }
-
-      if (filterConditions.length > 0) {
-        query += ` WHERE ${filterConditions.join(' AND ')}`;
-      }
-
-      // Apply sorting
-      query += ` ORDER BY ${sortField} ${sortOrder}`;
-
-      // Apply pagination
-      const offset = (page - 1) * limit;
-      query += ` LIMIT ? OFFSET ?`;
-      params.push(limit, offset);
-
-      const [rows] = await pool.query(query, params);
-      return rows;
-    } catch (error) {
-      console.error('error:', error);
-      throw error;
-    }
-  }
-
-  static async findById(id) {
-    const query = `SELECT dep.id, dep.name,
-      dep.capital_name
-      FROM department dep
-      WHERE dep.id = ?
-    `;
-    try {
-      const [rows] = await pool.query(query, [id]);
-      if (rows && rows.length > 0) {
-        return rows[0];
-      }
-      throw new Error('Department not found');
-    } catch (error) {
-      console.error('Error in Department.findById:', error);
-      throw error;
-    }
-  }
-
   static async findByLngLat(lng, lat) {
     const query = `SELECT dep.name, dep.capital_name,
         dis.name, ciu.name, ba.name
